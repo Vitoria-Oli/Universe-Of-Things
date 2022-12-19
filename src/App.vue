197a894ebb24e4eeeb06fd34dc7712772d3a513e
GabriMF <108740750+GabriMF@users.noreply.github.com>
2022-12-19 12:01:24 +0100

    sass donete

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,12 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    
     <h1>WE MASTER YOUR EXPERIENCE</h1>
     <div class="wrapper">
-
-      <!-- <nav>
+      <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">Favorites</RouterLink>
-      </nav> -->
+      </nav>
     </div>
   </header>
   
@@ -19,14 +17,17 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped lang="scss">
+@use "../src/assets/scss/default" as *;
+
 @font-face {
   font-family: Title;
   src: url(./assets/Shrikhand/Shrikhand-Regular.ttf);
 
 }
+
 header {
 
-  background-color: #E34038;
+  background-color: $TertiaryColor;
   width:95% ;
   height: 20%;
   margin: auto;
@@ -36,40 +37,12 @@ header {
   border-top-right-radius: 40px 40px;
   border-bottom-left-radius: 40px 40px;
   justify-content: center;
-}
 
-h1{
-  color: #312D2D;
-  font-family: Title;
-  font-size: 300%;
-
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-  justify-content: end;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-  justify-content: end;
-}
-
-nav a:first-of-type {
-  border: 0;
+  h1{
+    color: $PrincipalColor;
+    font-family: Title;
+    font-size: 300%;
+  }
 }
 
 @media (min-width: 1024px) {
@@ -77,25 +50,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
