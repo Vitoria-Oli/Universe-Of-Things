0571099d3fe53764525c0c918f134bd1308ff14b
GabriMF <108740750+GabriMF@users.noreply.github.com>
2022-12-21 16:31:30 +0100

    code cleaned

--- src/components/FavButton.vue
+++ src/components/FavButton.vue
@@ -1,34 +1,26 @@
 <script setup>
 
-
-import { computed } from 'vue';
  const props = defineProps ({
   name:{
     type: String,
     required: true,
-    
-
   },
   typeButton:{
     type: String,
     default: 'like',
   }
-
  })
- 
 </script>
 
 <template>
-      <button class="button">{{name}}</button>
- 
+  <button class="button">{{name}}</button>
 </template>
 
 <style  lang="scss" scoped >
- 
 @use '../assets/scss/button' as *;
+
 a {
   background: none;
   border: none;
 }
-
 </style>
